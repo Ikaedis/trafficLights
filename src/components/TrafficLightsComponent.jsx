67934ef1628a13101e27bfe9cdd5cac9c0b716e3
Sebastian Alvarez <corosito2002@gmail.com>
2024-06-13 09:21:33 -0500

    Added the JSX and CSS for the TrafficLingtsComponet, no functionality just yet, just html and css

---
+++ src/components/TrafficLightsComponent.jsx
@@ -0,0 +1,13 @@
+import "./TrafficLightsComponent.css"; // Importing the CSS file
+
+const TrafficLightsComponent = () => {
+  return (
+    <div className="boxes-container">
+      <div className="box red"></div>
+      <div className="box blue"></div>
+      <div className="box green"></div>
+    </div>
+  );
+};
+
+export default TrafficLightsComponent;
