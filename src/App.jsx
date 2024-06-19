c0fcfa4a4960f614028ba968c55c2b7a0d3f55c0
Sebastian Alvarez <corosito2002@gmail.com>
2024-06-18 19:49:01 -0500

    Some shit I don't need, I just wanna change branches

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import TrafficLightsComponent from "./components/TrafficLightsComponent";
 
 function App() {
+  const simpleArray = [1, 2, 3];
   return (
     <>
       <h1>This text only appears in the testing 1 branch</h1>
-      <TrafficLightsComponent />
+      <TrafficLightsComponent arrayToUse={simpleArray} />
     </>
   );
 }
--- src/components/TrafficLightsComponent.jsx
+++ src/components/TrafficLightsComponent.jsx
@@ -1,11 +1,62 @@
 import "./TrafficLightsComponent.css"; // Importing the CSS file
+import { useEffect, useState } from "react";
+
+const greenConfig = {
+  backgroundColor: "green",
+  next: "yellowConfig",
+};
+
+const yellowConfig = {
+  backgroundColor: "yellow",
+  next: "redConfig",
+};
+
+const redConfig = {
+  backgroundColor: "red",
+  next: "greenConfig",
+};
+
+const arrayOfLightsConfigs = [greenConfig, yellowConfig, redConfig];
+
+function getConfigByName(configName) {
+  switch (configName) {
+    case "greenConfig":
+      return greenConfig;
+    case "yellowConfig":
+      return yellowConfig;
+    case "redConfig":
+      return redConfig;
+    default:
+      throw new Error("Invalid configuration name");
+  }
+}
 
 const TrafficLightsComponent = () => {
+  const [currentColor, setCurrentColor] = useState(
+    arrayOfLightsConfigs[0].backgroundColor
+  );
+
+  useEffect(() => {}, [currentColor]);
+
   return (
     <div className="boxes-container">
-      <div className="box red"></div>
-      <div className="box blue"></div>
-      <div className="box green"></div>
+      {arrayOfLightsConfigs.map((colorConfig) => {
+        return (
+          <div
+            key={colorConfig.backgroundColor}
+            className={"trafficLight"}
+            style={{
+              backgroundColor:
+                currentColor ===
+                arrayOfLightsConfigs[colorConfig].backgroudColor
+                  ? colorConfig.backgroundColor
+                  : undefined,
+            }}
+          >
+            light
+          </div>
+        );
+      })}
     </div>
   );
 };
